c0f3c2e7966ed4200d44f3dce16d80bac4d351e4
Jonas Otto <jottosmail@gmail.com>
2020-01-03 16:54:21 +0100

    startgame message

--- inc/network/messages/client/StartGame.h
+++ inc/network/messages/client/StartGame.h
@@ -6,11 +6,19 @@
 #define HEXXAGON_STARTGAME_H
 
 
+#include <UUID.h>
 #include "network/messages/Message.h"
 
-class StartGame : Message {
+class StartGame : public Message {
+public:
+    StartGame();
 
+    UUID userId;
+    UUID lobbyId;
 };
 
+void to_json(nlohmann::json &j, const StartGame &s);
+
+void from_json(const nlohmann::json &j, StartGame &s);
 
 #endif //HEXXAGON_STARTGAME_H
---
+++ src/messages/client/StartGame.cpp
@@ -0,0 +1,19 @@
+//
+// Created by jonas on 30.12.19.
+//
+
+#include <network/messages/client/StartGame.h>
+
+void to_json(nlohmann::json &j, const StartGame &s) {
+    j["messageType"] = s.messageType;
+    j["userId"] = s.userId;
+    j["lobbyId"] = s.lobbyId;
+}
+
+void from_json(const nlohmann::json &j, StartGame &s) {
+    j.at("messageType").get_to(s.messageType);
+    j.at("userId").get_to(s.userId);
+    j.at("lobbyId").get_to(s.lobbyId);
+}
+
+StartGame::StartGame() : Message(MessageType::StartGame) {}
